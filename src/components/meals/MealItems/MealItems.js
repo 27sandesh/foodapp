36c16018d2d592e6674d6ac8e027a70ef1b707df
27sandesh <kamblesandesh254@gmail.com>
2023-04-24 11:38:48 +0530

    input

--- src/components/meals/MealItems/MealItems.js
+++ src/components/meals/MealItems/MealItems.js
@@ -1,4 +1,5 @@
 import classes from "./MealItems.module.css";
+import MealItemform from "./MealItemform";
 const MealItem = (props) => {
   const price = `$${props.price.toFixed(2)}`;
   return (
@@ -6,6 +7,9 @@ const MealItem = (props) => {
       <h3>{props.name}</h3>
       <div className={classes.description}>{props.description}</div>
       <div className={classes.price}>{price}</div>
+      <div>
+        <MealItemform />
+      </div>
     </li>
   );
 };
